16d29176d2aaceee527b058d796b02babd614b3f
Igor Ivanov <ivanov.i@gmail.com>
2018-07-22 02:54:58 +0700

    lines drawing

--- ellipse_animated.py
+++ ellipse_animated.py
@@ -1,5 +1,6 @@
 import scene
 import ui
+import math
 
 
 class MyScene (scene.Scene):
@@ -20,9 +21,16 @@ class MyScene (scene.Scene):
       self.lines = []
       point = self.size / 2 + scene.Point(100, 0)
       self.add_child(scene.LabelNode('x', position = point))
-      
- #     for angle in range(0, 360, 10):
-  #      add_line(angle)
+      for angle in range(0, 360, 10):
+        self.add_line(angle)
+        
+    def add_line(self, angle):
+      p = ui.Path.rect(0,0, 1000, 1)
+      line = scene.ShapeNode(p, stroke_color='white', fill_color= 'none')
+      line.anchor_point = (0,0)
+      line.rotation = math.radians(angle)
+      line.position = self.size /2
+      self.add_child(line)
 
     def update(self):
       pass
