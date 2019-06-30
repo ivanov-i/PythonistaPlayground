1579a53bcff2adc0b2d48c4377964add6bd8c455
Ivanov <ivanov.i@gmail.com>
2019-06-30 15:22:08 +0700

    Implement nearest neighbour correctly

--- NearestNeighbour.py
+++ NearestNeighbour.py
@@ -1,20 +1,30 @@
+import matplotlib.pyplot as plt
 import itertools
 import math
+import collections
+import random
+
 
 def p(x):
 	print(list(x))
 
-N = 10
-D = 3
+N = 10000
+D = 10000
 
-x = [x/N for x in range(0, N+1)]
+def createRandomPoint():
+	return list((random.random() for i in range(0, D)))
 
-points = list(itertools.combinations_with_replacement(x, D))
-pairs = itertools.combinations_with_replacement(points,2)
+pairs = ((createRandomPoint(), createRandomPoint()) for i in range(0, N))
+
 
 def distance(a,b):
-	return math.sqrt(sum([(x1 - x2)**2 for (x1, x2) in zip(a,b)]))
-	
-distances = itertools.chain([distance(first, second) for (first, second) in pairs if(first != second)])
+	return math.sqrt(sum(((x1 - x2)**2 for (x1, x2) in zip(a,b))))
 
-print(list(distances))
+distances = (round(distance(first, second), 2) for (first, second) in pairs)
+
+counts = collections.Counter(distances)
+
+labels, values = zip(*sorted(counts.items()))
+
+plt.plot(labels, values)
+plt.show()
